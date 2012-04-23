1c81ea6222bc9a9f57e8272a83bbaba567965fc2
Lanvige Jiang <lanvige@gmail.com>
2012-04-23 16:36:42 +0800

    Initial Commit

---
+++ Landmark/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Landmark
+//
+//  Created by Lanvige Jiang on 4/23/12.
+//  Copyright (c) 2012 LANVIGE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ Landmark/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Landmark
+//
+//  Created by Lanvige Jiang on 4/23/12.
+//  Copyright (c) 2012 LANVIGE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end

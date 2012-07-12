ee585998fb4dfed541aa9360e8fded7b06fc84a4
Lanvige Jiang <lanvige@gmail.com>
2012-07-12 23:10:57 +0800

    add new contact model

---
+++ Landmark/LMContact.m
@@ -0,0 +1,13 @@
+//
+//  LMContact.m
+//  Landmark
+//
+//  Created by Lanvige Jiang on 7/12/12.
+//  Copyright (c) 2012 LANVIGE. All rights reserved.
+//
+
+#import "LMContact.h"
+
+@implementation LMContact
+
+@end
---
+++ Landmark/LMSetting.h
@@ -0,0 +1,13 @@
+//
+//  LMSetting.h
+//  Landmark
+//
+//  Created by Lanvige Jiang on 7/12/12.
+//  Copyright (c) 2012 LANVIGE. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LMSetting : NSObject
+
+@end

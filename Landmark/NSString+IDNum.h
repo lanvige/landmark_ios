bc3c12e8d1f8d1246247c1101c51be5610526238
Lanvige Jiang <lanvige@gmail.com>
2012-08-04 14:53:54 +0800

    add some useful categories to project

---
+++ Landmark/NSString+IDNum.h
@@ -0,0 +1,15 @@
+//
+//  NSString+IDNum.h
+//  Efekta12NavigationTest
+//
+//  Created by Lin Shi on 7/9/12.
+//  Copyright (c) 2012 EF Englishtown. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (IDNum)
+
+- (NSString*)numberStringOfID;
+
+@end
---
+++ Landmark/UILabel+Resize.h
@@ -0,0 +1,15 @@
+//
+//  UIView+Resize.h
+//  Efekta12NavigationTest
+//
+//  Created by Lanvige Jiang on 7/24/12.
+//  Copyright (c) 2012 EF Englishtown. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UILabel(Resize) 
+
+- (CGFloat) resizeLabel;
+
+@end
